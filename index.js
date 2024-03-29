957386af4bc1da846b86702cabe7ab8925033948
Victor <victorac2324@gmail.com>
2024-03-29 13:03:29 -0400

    used arithmatic to set variables

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+// let random = Math.random()
+
+// Math.ceil(random)
+//answer but not what test wanted 
+
+// function random(){
+//     let num3 = Math.random()
+
+//     return Math.ceil(++num3)}
+
+
+//asnwer via IA
+let random = Math.ceil(Math.random());
+
+
+let num3 = 93657
+let num4 = 7
+let mod = num3 % num4
+let max = (8, 4, 7, 17, 20)
